152737b551da8d6c920c4b892a8939d3018dd0a7
Bendiconaccento <alessiodellarocca@gmail.com>
2024-09-27 17:54:36 +0200

    Update Cioccolato.py

--- Cioccolato.py
+++ Cioccolato.py
@@ -13,3 +13,17 @@ class Cioccolato:
         print(f"Il tipo di cioccolato è: {self.tipo}, la percentuale di cacao è {self.percentuale}")
 
 
+class Cioccolatino(Cioccolato):
+
+    def __init__(self, forma, ripieno, tipo, percentuale):
+
+        super().__init__(tipo, percentuale)
+        self.forma = forma
+        self.ripieno = ripieno
+
+        self.costo_cioccolato = 2
+
+    def produce(self):
+        
+        super().produce()
+        print(f"La forma è: {self.forma}, il ripieno è {self.ripieno}, consuma {self.costo_cioccolato} unità.")
