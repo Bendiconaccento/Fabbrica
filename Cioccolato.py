b9d6e3ac317a8a2f76972c7e6ef2503655b8adab
Bendiconaccento <alessiodellarocca@gmail.com>
2024-09-27 17:55:51 +0200

    Update Cioccolato.py

--- Cioccolato.py
+++ Cioccolato.py
@@ -13,3 +13,18 @@ class Cioccolato:
         print(f"Il tipo di cioccolato è: {self.tipo}, la percentuale di cacao è {self.percentuale}")
 
 
+class Tavoletta(Cioccolato):
+
+    def __init__ (self, peso, tipo, percentuale, aggiunte=None ):
+
+        super().__init__(tipo, percentuale)
+        self.peso = peso
+        self.aggiunte = aggiunte or []
+
+        self.costo_cioccolato = max(4, min(24, peso // 10))
+
+    def produce(self):
+
+        super().produce()
+        aggiunte_descrizione = ",".join(self.aggiunte) if self.aggiunte else "nessuna aggiunta"
+        print(f"Il peso è: {self.peso}g, Con aggiunta di: {aggiunte_descrizione}. Consuma {self.costo_cioccolato} unità di cioccolato.")
